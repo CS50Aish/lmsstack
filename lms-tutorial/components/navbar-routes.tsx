f717fae3f06139a4f3e512f46a94f4e8ba863a65
Ubuntu <labuser@ubuntu2204.rggagp4wfayurg3qhm2tcxnxmh.rx.internal.cloudapp.net>
2024-07-27 10:56:55 +0000

    Updated Files

--- lms-tutorial/components/navbar-routes.tsx
+++ lms-tutorial/components/navbar-routes.tsx
@@ -3,7 +3,8 @@
 import { UserButton } from "@clerk/nextjs";
 import { usePathname, useRouter } from "next/navigation";
 import { Button } from "./ui/button";
-import { Link, LogOut } from "lucide-react";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
 
@@ -16,18 +17,20 @@ export const NavbarRoutes = () => {
     return (
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
-                <Button>
+                <Button size="sm" variant="ghost">
                     <LogOut className="h-4 w-4 mr-2"/>
                     Exit
                 </Button>
             ) : (
                 <Link href="/teacher/courses">
-                    <Button>
+                    <Button size="sm" variant="ghost">
                         Teacher Mode
                     </Button>
                 </Link>
             )}
-            <UserButton />
+            <UserButton 
+                afterSignOutUrl="/"
+            />
         </div>
     )
 }
